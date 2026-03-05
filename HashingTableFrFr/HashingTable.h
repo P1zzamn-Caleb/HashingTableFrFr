10d63b24e60675e0a1906175208c595b6fb707a5
goodroejay-dotcom <goodroejay@gmail.com>
2026-03-05 12:21:48 -0600

    Update HashingTable.h
    working on setting up methods in header file

--- HashingTableFrFr/HashingTable.h
+++ HashingTableFrFr/HashingTable.h
@@ -1,4 +1,18 @@
 #ifndef _HASHING_TABLE_H
 #define _HASHING_TABLE_H
 
+// constructor
+HashTable();
+// insert 
+void Insert();
+// remove
+bool Remove();
+// print
+void Print();
+// find
+bool Search();
+
+// array of keys
+// actual data for each key (linked list)                                           
+
 #endif
